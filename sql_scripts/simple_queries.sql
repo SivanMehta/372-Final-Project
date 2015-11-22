be42def70b3b0cd7c837e7cbdf64039339ee403d
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-11-22 18:11:46 -0500

    added some customer queries

--- sql_scripts/simple_queries.sql
+++ sql_scripts/simple_queries.sql
@@ -4,17 +4,26 @@
 
 \echo 'Community Admin'
 \echo 'Get average rating for each driver'
--- get the average rating for each driver
 SELECT concat(first_name, ' ', last_name) AS name, round(avg(rating),2)
 FROM users
 INNER JOIN ratings ON users.userid = ratings.userid
 WHERE users.role = 'driver'
 GROUP BY name;
 
--- get the average rating for each customer
 \echo 'Get average rating for each customers'
 SELECT concat(first_name, ' ', last_name) AS name, round(avg(rating),2)
 FROM users
 INNER JOIN ratings ON users.userid = ratings.userid
 WHERE users.role = 'customer'
 GROUP BY name;
+
+\echo 'show customer #8’s credit cards'
+SELECT RIGHT(credit_card_number, 4) as "Last four digits", expiration as "Exp"
+FROM CreditCards
+WHERE userID = 8; -- really can be any user. user 8 just has a couple
+
+\echo 'see all drivers on the road'
+SELECT first_name as "Name", cars.picture, Cars.make as "Make", Cars.model as "Model", Cars.year as "Year", Cars.color as "Color", Cars.picture, Cars.seats as "Seats", Cars.status as "Tier"
+FROM users
+INNER JOIN Cars ON users.userid = Cars.userid
+WHERE users.role = 'driver';
