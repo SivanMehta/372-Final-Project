61989aaa5ab8193d71a5047b2e2a3494c6828e71
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-11-22 17:45:36 -0500

    added queries community admin

--- sql_scripts/simple_queries.sql
+++ sql_scripts/simple_queries.sql
@@ -1,8 +1,20 @@
 \c uber
 
--- get total number of trips done for each driver
-select first_name, last_name, count(tripid)
-from users
-inner join trips
-on users.userid = trips.driverid
-group by users.userid;
+-- These are the queries corresponding to each user story
+
+\echo 'Community Admin'
+\echo 'Get average rating for each driver'
+-- get the average rating for each driver
+SELECT concat(first_name, ' ', last_name) AS name, round(avg(rating),2)
+FROM users
+INNER JOIN ratings ON users.userid = ratings.userid
+WHERE users.role = 'driver'
+GROUP BY name;
+
+-- get the average rating for each customer
+\echo 'Get average rating for each customers'
+SELECT concat(first_name, ' ', last_name) AS name, round(avg(rating),2)
+FROM users
+INNER JOIN ratings ON users.userid = ratings.userid
+WHERE users.role = 'customer'
+GROUP BY name;
