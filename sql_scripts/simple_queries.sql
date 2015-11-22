26bf76cbb80c6f130d0c418befc4ad6b16bd6d1e
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-11-22 17:04:42 -0500

    added query get the amount of trips each driver has done

---
+++ sql_scripts/simple_queries.sql
@@ -0,0 +1,8 @@
+\c
+
+-- get total number of trips done for each driver
+select first_name, last_name, count(tripid)
+from users
+inner join trips
+on users.userid = trips.driverid
+group by users.userid;
