4dfd1f46c4608d743fff7fff53ba6f299a0f035a
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-12-01 12:36:44 -0500

    added aliases for example queries

--- sql_scripts/simple_queries.sql
+++ sql_scripts/simple_queries.sql
@@ -4,14 +4,14 @@
 
 \echo 'Community Admin'
 \echo 'Get average rating for each driver'
-SELECT concat(first_name, ' ', last_name) AS name, round(avg(rating),2)
+SELECT concat(first_name, ' ', last_name) AS name, round(avg(rating),2) as "rating"
 FROM users
 INNER JOIN ratings ON users.userid = ratings.userid
 WHERE users.role = 'driver'
 GROUP BY name;
 
-\echo 'Get average rating for each customers'
-SELECT concat(first_name, ' ', last_name) AS name, round(avg(rating),2)
+\echo 'Get average rating for each customer'
+SELECT concat(first_name, ' ', last_name) AS name, round(avg(rating),2) as "rating"
 FROM users
 INNER JOIN ratings ON users.userid = ratings.userid
 WHERE users.role = 'customer'
@@ -22,7 +22,7 @@ SELECT RIGHT(credit_card_number, 4) as "Last four digits", expiration as "Exp"
 FROM CreditCards
 WHERE userID = 8; -- really can be any user. user 8 just has a couple
 
-\echo 'see all drivers on the road'
+\echo 'see all drivers'
 SELECT first_name as "Name", cars.picture, Cars.make as "Make", Cars.model as "Model", Cars.year as "Year", Cars.color as "Color", Cars.picture, Cars.seats as "Seats", Cars.status as "Tier"
 FROM users
 INNER JOIN Cars ON users.userid = Cars.userid
