85aa8e781b7ddc9abeca17cc6a13d9479d830c4b
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-12-01 11:09:04 -0500

    added trigger for new trips

--- sql_scripts/create.sql
+++ sql_scripts/create.sql
@@ -72,3 +72,15 @@ CREATE TABLE Ratings
   userID      int REFERENCES Users (userID),
   tripID      int REFERENCES Trips (tripID)
 );
+
+--------------
+-- Triggers --
+--------------
+
+CREATE FUNCTION trip_stamp() RETURNS trigger as $_$
+BEGIN
+    update Trips
+    set    trip_timestamp = NOW()
+    where  tripID = new.tripID;
+    return new;
+END $_$ LANGUAGE 'plpgsql'
