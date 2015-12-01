a6450094b9dc8b11e836eb0014b842ab42667f65
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-12-01 11:46:35 -0500

    data2 doesn't exist

--- sql_scripts/initialize.sql
+++ sql_scripts/initialize.sql
@@ -1,10 +1,10 @@
 \c uber
 
-\copy users(first_name, last_name, picture, role)                           FROM 'data2/Users.csv'        delimiter ',' csv header
-\copy cars(userID, make, model, year, color, picture, seats, status)        FROM 'data2/Cars.csv'         delimiter ',' csv header
-\copy creditcards(credit_card_number, expiration, cvv2, userID)            FROM 'data2/Credit_cards.csv' delimiter ',' csv header
-\copy trips(trip_timestamp,duration,pickup,destination,fare,driverID,credit_cardID)       FROM 'data2/Trips.csv'        delimiter ',' csv header
-\copy ratings(rating,userID,tripID)     FROM 'data2/Ratings.csv'      delimiter ',' csv header
+\copy users(first_name, last_name, picture, role)                           FROM 'data/Users.csv'        delimiter ',' csv header
+\copy cars(userID, make, model, year, color, picture, seats, status)        FROM 'data/Cars.csv'         delimiter ',' csv header
+\copy creditcards(credit_card_number, expiration, cvv2, userID)            FROM 'data/Credit_cards.csv' delimiter ',' csv header
+\copy trips(trip_timestamp,duration,pickup,destination,fare,driverID,credit_cardID)       FROM 'data/Trips.csv'        delimiter ',' csv header
+\copy ratings(rating,userID,tripID)     FROM 'data/Ratings.csv'      delimiter ',' csv header
 
 --------------
 -- Triggers --
