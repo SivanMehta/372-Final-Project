6082f6fce225e0b7aba79a9a181333aeed4f51f9
Sivan Mehta <skmehta@andrew.cmu.edu>
2015-12-06 18:49:56 -0500

    added some helpers for the complex queries to use

---
+++ python_scripts/helpers.py
@@ -0,0 +1,42 @@
+import psycopg2
+import math
+
+conn = psycopg2.connect(database = "uber", user = "skmehta", host = "localhost", port = "5432")
+cur = conn.cursor()
+conn.autocommit = True
+
+# Great Circle distance between two (latitude, longitude) pairs
+# http://www.johndcook.com/blog/python_longitude_latitude/
+
+def distance(position1, position2):
+ 
+    lat1, long1 = position1
+    lat2, long2 = position2
+
+    # Convert latitude and longitude to 
+    # spherical coordinates in radians.
+    degrees_to_radians = math.pi/180.0
+         
+    # phi = 90 - latitude
+    phi1 = (90.0 - lat1)*degrees_to_radians
+    phi2 = (90.0 - lat2)*degrees_to_radians
+         
+    # theta = longitude
+    theta1 = long1*degrees_to_radians
+    theta2 = long2*degrees_to_radians
+         
+    # Compute spherical distance from spherical coordinates.
+         
+    # For two locations in spherical coordinates 
+    # (1, theta, phi) and (1, theta', phi')
+    # cosine( arc length ) = 
+    #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+    # distance = rho * arc length
+     
+    cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) + 
+           math.cos(phi1)*math.cos(phi2))
+    arc = math.acos( cos )
+ 
+    # Remember to multiply arc by the radius of the earth 
+    # in your favorite set of units to get length.
+    return arc * 3959
